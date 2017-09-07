4732a1a97b93454ccc6b8a43628495bc61d4dc98
kadek <kadek@dominopos.com>
2017-09-07 12:10:06 +0800

    project 1

---
+++ project1/data.py
@@ -0,0 +1,25 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': 'Apakah Lorem Ipsum itu?',
+			'author': 'kadek',
+			'body': 'Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting. Lorem Ipsum telah menjadi standar contoh teks sejak tahun 1500an, saat seorang tukang cetak yang tidak dikenal mengambil sebuah kumpulan teks dan mengacaknya untuk menjadi sebuah buku contoh huruf. Ia tidak hanya bertahan selama 5 abad, tapi juga telah beralih ke penataan huruf elektronik, tanpa ada perubahan apapun. Ia mulai dipopulerkan pada tahun 1960 dengan diluncurkannya lembaran-lembaran Letraset yang menggunakan kalimat-kalimat dari Lorem Ipsum, dan seiring munculnya perangkat lunak Desktop Publishing seperti Aldus PageMaker juga memiliki versi Lorem Ipsum.',
+			'created_date': '04-25-2017'
+		},
+		{
+			'id': 2,
+			'title': 'Dari mana asalnya?',
+			'author': 'kadek',
+			'body': 'Tidak seperti anggapan banyak orang, Lorem Ipsum bukanlah teks-teks yang diacak. Ia berakar dari sebuah naskah sastra latin klasik dari era 45 sebelum masehi, hingga bisa dipastikan usianya telah mencapai lebih dari 2000 tahun. Richard McClintock, seorang professor Bahasa Latin dari Hampden-Sidney College di Virginia, mencoba mencari makna salah satu kata latin yang dianggap paling tidak jelas, yakni consectetur, yang diambil dari salah satu bagian Lorem Ipsum.',
+			'created_date': '05-30-2017'
+		},
+		{
+			'id': 3,
+			'title': 'Mengapa kita menggunakannya?',
+			'author': 'kadek',
+			'body': 'Sudah merupakan fakta bahwa seorang pembaca akan terpengaruh oleh isi tulisan dari sebuah halaman saat ia melihat tata letaknya. Maksud penggunaan Lorem Ipsum adalah karena ia kurang lebih memiliki penyebaran huruf yang normal, ketimbang menggunakan kalimat seperti "Bagian isi disini, bagian isi disini", sehingga ia seolah menjadi naskah Inggris yang bisa dibaca.',
+			'created_date': '06-31-2017'
+		}
+	]
+	return articles
