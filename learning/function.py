5182ecddbd28974f0179108ac1bfcab70880fb64
kadek bagus <kadekdarma90@gmail.com>
2017-09-09 23:06:39 +0800

    add print.py and function.py on learning folder

---
+++ learning/function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# function myname
+def myname (name) :
+	print name
+	return
+
+myname("kadek ganteng")
+myname("kadek darma")
+
+# function calc
+def calc(param1, param2):
+	return param1*param2
+
+print(calc(1,5))
+
+
+
+
